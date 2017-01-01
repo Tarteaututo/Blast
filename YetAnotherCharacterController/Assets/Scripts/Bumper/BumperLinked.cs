81756c5101514a5dc236da533d20f50135a7ebf7
Fand-PC\Fand <alexandre.lattavo@gmail.com>
2017-01-01 17:10:20 +0100

    Bumper has Timer géré. Multiple loader pour les bumpers géré aussi. A faire : feedback de timer pour les bumpers.

--- YetAnotherCharacterController/Assets/Scripts/Bumper/BumperLinked.cs
+++ YetAnotherCharacterController/Assets/Scripts/Bumper/BumperLinked.cs
@@ -7,7 +7,7 @@ public class BumperLinked : Bumper {
 	protected override void Start() {
 		base.Start();
 
-		if (this.hasTimer) {
+		if (this.hasTimer && !this.isLinked) {
 			StartCoroutine(OnSelfTimer());
 		}
 	}
@@ -15,12 +15,12 @@ public class BumperLinked : Bumper {
 	IEnumerator OnSelfTimer() {
 		yield return new WaitForSeconds(this.timer);
 
-		this.SwitchByLoader(!this.isBumpActive);
+		this.SelfSwitch(!this.isBumpActive);
 
 		yield return OnSelfTimer();
 	}
 
-	public void SwitchByLoader(bool isActivate) {
+	public void SelfSwitch(bool isActivate) {
 		
 		if (!this.isActiveAtStart) {
 			isActivate = !isActivate;
@@ -34,9 +34,29 @@ public class BumperLinked : Bumper {
 	}
 
 	public void NewSwitchByLoader() {
+		if (this.isOnBump)
+			return;
 		this.isBumpActive = !this.isBumpActive;
 
 		this.isOnBump = true;
 		StartCoroutine(SetActivation());
+		
+		if (this.hasTimer)
+			StartCoroutine(HandleLocalTimer());
+	}
+
+	IEnumerator HandleLocalTimer() {
+		yield return new WaitForSeconds(this.timer);
+		this.AfterTimerSwitchByLoader();
+	}
+
+	public void AfterTimerSwitchByLoader() {
+		if (this.isOnBump)
+			return;
+		this.isBumpActive = !this.isBumpActive;
+
+		this.isOnBump = true;
+		StartCoroutine(SetActivation());
+
 	}
 }
