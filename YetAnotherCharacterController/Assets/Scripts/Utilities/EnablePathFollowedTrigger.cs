ce8a062d3d07264a298e2e50d0bc96bdc820734d
Fand-PC\Fand <alexandre.lattavo@gmail.com>
2017-01-06 22:31:57 +0100

    Main menu ok. Sortir la build avec les inputs maintenant. Petit tuto sur le retry aussi.

---
+++ YetAnotherCharacterController/Assets/Scripts/Utilities/EnablePathFollowedTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnablePathFollowedTrigger : MonoBehaviour {
+	public NewPathFollowedPlatform target;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+			this.ToggleEnable();
+		}
+	}
+
+	void ToggleEnable() {
+		this.target.enabled = !this.target.enabled;
+	}
+}
