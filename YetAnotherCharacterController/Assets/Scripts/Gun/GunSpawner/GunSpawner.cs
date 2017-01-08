96915a31242c49d85d336c3ab9622e77d024ce18
Fand-PC\Fand <alexandre.lattavo@gmail.com>
2017-01-08 22:42:42 +0100

    GunSwitch particle polish ok. Faire TriggerDegradePooler.

--- YetAnotherCharacterController/Assets/Scripts/Gun/GunSpawner/GunSpawner.cs
+++ YetAnotherCharacterController/Assets/Scripts/Gun/GunSpawner/GunSpawner.cs
@@ -2,24 +2,35 @@
 using System.Collections;
 
 public class GunSpawner : MonoBehaviour {
-	Transform pivot;
 	[SerializeField] TriggerGun.GunMode gunMode;
+
+	[SerializeField] float timeEffectGetGun = 1f;
+	Transform pivot;
+	Transform feedbackParticle;
+	bool isGunGiven = false;
 
 	void Awake() {
 		this.pivot = this.transform.FindChild("Pivot");
+		this.feedbackParticle = this.transform.FindChild("Feedback");
 	}
 
 	void Update() {
-		this.pivot.transform.Rotate(Vector3.up);
+		if (!this.isGunGiven)
+			this.pivot.transform.Rotate(Vector3.up);
 	}
 
 	void OnTriggerEnter(Collider other) {
+		if (this.isGunGiven)
+			return;
+		
 		if (other.CompareTag("Player")) {
 			this.GiveGunToPlayer(other.GetComponent<TriggerGun>());
 		}
 	}
 
 	void GiveGunToPlayer(TriggerGun triggerGun) {
+		this.isGunGiven = true;
+
 		switch (gunMode) {
 			case TriggerGun.GunMode.SWITCH:
 				triggerGun.canSwitchGun = true;
@@ -34,5 +45,30 @@ public class GunSpawner : MonoBehaviour {
 			default:
 				break;
 		}
+
+		this.pivot.gameObject.SetActive(false);
+		this.PlayFeedbackParticles();
+		StartCoroutine(this.OnCoolEffect(triggerGun.gameObject));
+	}
+
+	void PlayFeedbackParticles() {
+		ParticleSystem[] particles = this.feedbackParticle.GetComponentsInChildren<ParticleSystem>();
+
+		foreach (ParticleSystem element in particles) {
+			if (element.name == "ParticlesIdle") {
+				element.Stop();
+			} else if (element.name == "ParticlesGetGun") {
+				element.Play();
+			}
+		}
+	}
+
+	IEnumerator OnCoolEffect(GameObject player) {
+		//Time.timeScale = 0.5f;
+		yield return new WaitForSeconds(this.timeEffectGetGun);
+
+		//Time.timeScale = 1f;
+
+
 	}
 }
