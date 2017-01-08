1177e9a5df01f41d3349548aa45a4fcc771efaf1
Fand-PC\Fand <alexandre.lattavo@gmail.com>
2017-01-08 23:25:30 +0100

    TriggerDegradePooler ok.

---
+++ YetAnotherCharacterController/Assets/Resources/Prefabs/Pooler/TriggerDegradePooler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDegradePooler : MonoBehaviour {
+	public LinkedParticlePoolerSettings[] particlePooler;
+	bool isPoolerDegraded = false;
+
+	void OnTriggerEnter(Collider other) {
+		if (!this.isPoolerDegraded && other.CompareTag("Player")) {
+			this.DegradePooler();
+		}
+	}
+
+	void DegradePooler() {
+		this.isPoolerDegraded = true;
+
+		for (int i = 0; i < this.particlePooler.Length; i++) {
+			this.particlePooler[i].Load(true);
+		}
+	}
+}
